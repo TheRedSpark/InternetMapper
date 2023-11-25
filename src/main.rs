b6a445022fb6fc3a3670e8da1d41a6c066914f12
erre893d <eric.reichelt1@mailbox.tu-dresden.de>
2023-11-25 16:00:39 +0100

    Aufteilung in eigene Funktionen

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
-
-
-
+use std::net::IpAddr;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    ping("127.0.0.1".parse().unwrap()).await
+}
+
+async fn ping(ipaddr: IpAddr) -> Result<(), Box<dyn std::error::Error>> {
     let payload = [0; 8];
 
-    let (_packet, duration) = surge_ping::ping("127.0.0.1".parse()?, &payload).await?;
-    surge_ping::Pinger::
+    let (_packet, duration) = surge_ping::ping(ipaddr, &payload).await?;
     println!("Ping took {:.3?}", duration);
 
     Ok(())
