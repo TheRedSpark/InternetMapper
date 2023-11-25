3f8e568d35ac01dc84c28ef548a0929b1877c337
erre893d <eric.reichelt1@mailbox.tu-dresden.de>
2023-11-25 16:40:15 +0100

    Nun können Ip-Ranges Gepingt werden

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,34 @@
-use std::net::IpAddr;
+use std::time::Duration;
+
+use ipnet::{Ipv4AddrRange, Ipv4Subnets};
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    ping("127.0.0.1".parse().unwrap()).await
-}
+async fn main() {
+    let hosts = Ipv4AddrRange::new(
+        "141.30.224.1".parse().unwrap(),
+        "141.30.224.254".parse().unwrap(),
+    );
+    let subnets = Ipv4Subnets::new(
+        "141.30.224.1".parse().unwrap(),
+        "141.30.224.254".parse().unwrap(),
+        24,
+    );
+    for host in hosts {
+        let duration: Duration = match ping(host.to_string()).await {
+            Ok(file) => file,
+            Err(_) => {
+                Duration::ZERO
+            }
+        };
+        println!("Der Host mit der Adresse:{} hat die Antwortzeit:{:?}", host.to_string(), duration);
+    }
 
-async fn ping(ipaddr: IpAddr) -> Result<(), Box<dyn std::error::Error>> {
-    let payload = [0; 8];
+    async fn ping(ipaddr: String) -> Result<Duration, Box<dyn std::error::Error>> {
+        let payload = [0; 8];
 
-    let (_packet, duration) = surge_ping::ping(ipaddr, &payload).await?;
-    println!("Ping took {:.3?}", duration);
+        let (_packet, duration) = surge_ping::ping(ipaddr.parse().unwrap(), &payload).await?;
+        //println!("Ping took {:.3?}", duration);
 
-    Ok(())
+        Ok(duration)
+    }
 }
